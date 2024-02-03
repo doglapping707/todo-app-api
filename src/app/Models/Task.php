eec28ad85bda7e35cee5e81f3a1d853916c8b35e
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-02-04 00:56:09 +0900

    Create Actions

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'is_done'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_done' => 'boolean',
+    ];
 }
