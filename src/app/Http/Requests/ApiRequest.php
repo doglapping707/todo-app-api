f05d95211f8e65251a3b5cba47fc5971852364c4
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-02-04 09:21:22 +0900

    Disabled redirection when validation error occurs.

---
+++ src/app/Http/Requests/ApiRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiRequest extends FormRequest
+{
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator  $validator
+     * @return void
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $response = [
+            'message' => 'Validation failed.',
+            'errors' => $validator->errors()
+        ];
+
+        throw new HttpResponseException(response()->json($response, 422));
+    }
+}
